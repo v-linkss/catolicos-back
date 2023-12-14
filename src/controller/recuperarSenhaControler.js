ca78b3597ca21db42b23aaae8ee203fbedd6402c
Ewerton Hugo <ewerton.hugo@souunit.com.br>
2023-12-14 18:02:19 -0300

    adicionando exeções

--- src/controller/recuperarSenhaControler.js
+++ src/controller/recuperarSenhaControler.js
@@ -7,25 +7,40 @@ class RecuperarSenhaController {
 
   async recuperarSenha(req, res) {
 
-      try {
-        const jsonData = req.body;
-        const resultado = await recuperarSenhaService.recuperarSenha(jsonData.email);
-        resultado = JSON.parse(resultado)
-        res.status(200).json(resultado);
-      } catch (error) {
-        res.status(500).json({ error: 'Erro ao recuperar senha' });
+    try {
+      const jsonData = req.body;
+
+      if (!jsonData || !jsonData.email || jsonData.email === "") {
+        return res.status(500).json({ error: "Invalid data. Data object should contain email." });
+
       }
+      const resultado = await recuperarSenhaService.recuperarSenha(jsonData.email);
+      resultado = JSON.parse(resultado)
+      res.status(200).json(resultado);
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao recuperar senha' });
     }
+  }
 
     async alterarSenha(req, res) {
       const jsonData = req.body;
 
-      try {
+      try { 
+
+        if (!jsonData || jsonData === "" ||
+            !jsonData.token || !jsonData.token === "" ||
+            !jsonData.senha_nova || jsonData.senha_nova === "" ||
+            !jsonData.versao || jsonData.versao === "") {
+            return res.status(500).json({ error: "Invalid data. Data object should contain token, senha_nova and versao." });
+
+        }
+
         const resultado = await recuperarSenhaService.alterarSenha(jsonData);
         res.status(200).json(resultado);
 
       } catch (error) {
-        res.status(500).json({ error: "Erro ao alterar a senha:", error });
+
+        res.status(500).json({ error: "Erro ao alterar a senha",  });
 
       }
     }
